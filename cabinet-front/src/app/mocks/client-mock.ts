73092e769a76fe11d08181e7ae89bdcc4d50595e
nabilus13 <nabiloumghar84@gmail.com>
2022-10-27 19:09:25 +0200

    adding mock and models

---
+++ cabinet-front/src/app/mocks/client-mock.ts
@@ -0,0 +1,11 @@
+export const commonFiedls = {
+  certificat: {
+    usage: 'testUsage',
+    typeOffre: 'testypeOfre',
+    cleStructurePerimetreContractuel: 'testCle',
+    domaine: [{ nom: 'test', extension: true, isValid: false }],
+    restriction: '',
+    editeurSolution: '',
+    solution: '',
+  },
+};
